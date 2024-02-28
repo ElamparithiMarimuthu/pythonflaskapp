259b9a5e8c1092d19d38efa6d3782dc79fca9251
ElamparithiMarimuthu <139229333+ElamparithiMarimuthu@users.noreply.github.com>
2024-02-28 11:44:42 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,50 @@
+from flask import Flask, render_template, redirect, url_for, request
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+# Replace 'mysql' with your database dialect if it's different (e.g., 'postgresql')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://Elamparithi:Elam1234@pythonflaskdb.cryu0guau2c1.us-east-2.rds.amazonaws.com:3306/pythonflaskdb'
+db = SQLAlchemy(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username, password=password).first()
+        if user:
+            login_user(user)
+            return redirect(url_for('gym_details'))
+    return render_template('login.html')
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+@app.route('/gym-details')
+@login_required
+def gym_details():
+    return render_template('gym_details.html')
+
+if __name__ == '__main__':
+    db.create_all()
+    app.run(debug=True)
